28d168f6524d646c26ed8c6607c0220765d07342
Anthony1911 <guspathan@student.gu.se>
2018-02-18 17:58:12 +0100

    Small fix

--- src/CarClass.java
+++ src/CarClass.java
@@ -7,96 +7,105 @@ public class CarClass implements CarInterface {
     public  int lane_pos=1 ;
     public ArrayList<Integer> data = new ArrayList<>();
     public  boolean isEmpty=true;
+   // public Point Coordinates = new Point (Current_postion,lane_pos );
     String error_message="";
 
     private Radar front_radar = new Radar(0, new ArrayList<>());
     private Radar rear_radar = new Radar(0, new ArrayList<>());
     private Radar left_radar = new Radar(0, new ArrayList<>());
     private Lidar lidar = new Lidar(0, new ArrayList<>());
+    private Actuator actuator = new Actuator();
 
-    public CarClass(int positionX, int positionY) {
-        this.current_position = positionX;
+    public CarClass(int positionx, int positionY) {
+        this.current_position = positionx;
         this.lane_pos = positionY;
     }
 
     // Assuming the car is moving 5 meters whenever the move function is called the car will move 5 meters. it stops  it reaches 100 meters.
     public boolean moveForward() {
-        if (current_position < 96 && current_position >= 0) {
-
-            current_position += 5;
-            return true;
-
-        } else {
-
-            return false;
-
-        }
+        boolean state = actuator.moveForward(this);
+        return state;
     }
 
-    public boolean leftLaneDetect(int front, int rear, int left, int lidar_data,
-                                  int front1, int rear1, int left1, int lidar_data1) {
+    
+    public boolean leftLaneDetect(int front, int rear, int left, int lidar_data, int front1, int rear1, int left1,
+			int lidar_data1) {
 
-        int count_changeLane = 0;
-        int count_faulty = 0;
-        int count_corrupted=0;
-        query(front, rear, left, lidar_data);
-        query(front1, rear1, left1, lidar_data1);
+		int count_changeLane = 0;
+		int count_faulty = 0;
+		int count_corrupted = 0;
+		querry(front, rear, left, lidar_data);
+		querry(front1, rear1, left1, lidar_data1);
 
-        for (int j = 0; j < data.size(); j++) {
+		for (int j = 0; j < data.size(); j++) {
 
-            if (j<4 && data.get(j) == 2 && (data.get(j+4) == 2 || data.get(j+4) == -1 || data.get(j+4) == 1) ) {
-                count_faulty++;
+			if (j < 4 && data.get(j) == 2 && (data.get(j + 4) == 2 || data.get(j + 4) == -1 || data.get(j + 4) == 1)) {
+				count_faulty++;
+			}
+			if (j < 4 && data.get(j) == 1 && data.get(j + 4) == 1) {
+				count_changeLane++;
 
-            }
-            if (j <4 && data.get(j) == 1 &&  data.get(j+4)==1 ) {
-                count_changeLane++;
+			}
+			if (j < 4 && data.get(j) == -1 && data.get(j + 4) == -1) {
+				count_corrupted++;
+			}
+		}
+		if (count_changeLane > 1 && count_faulty == 0) {
+			isEmpty = true;
 
-            } if (j < 4 && data.get(j) == -1 && data.get(j+4) == -1){
-                count_corrupted++;
-            }
+		} else if (count_faulty > 0 && isEmpty) {
+			isEmpty = false;
+		} else if (count_corrupted > 1 && count_changeLane < 2) {
+			isEmpty = false;
+			System.out.println("you have more than 1 corrupted sensor");
+			error_message = "you have more than 1 corrupted sensor";
 
+		}
+		System.out.println("enfjeje" + count_faulty + isEmpty + count_changeLane);
+		return isEmpty;
+	}
 
-        }
-        if (count_changeLane > 1 && count_faulty ==0  ) {
-            isEmpty = true;
+	public ArrayList<Integer> querry(int front, int rear, int left, int lidar_data) {
 
-        } else if (count_faulty > 0 &&  isEmpty){
-           isEmpty = false;
-        } else if (count_corrupted >1 && count_changeLane <2) {
-            isEmpty = false;
-            System.out.println("you have more than 1 corrupted sensor") ;
-            error_message="you have more than 1 corrupted sensor";
-        }
-        return isEmpty;
-    }
+		data.add(front_radar.checkReading(front));
+		data.add(rear_radar.checkReading(rear));
+		data.add(left_radar.checkReading(left));
+		data.add(lidar.checkReading(lidar_data));
 
-    public ArrayList<Integer> query(int front, int rear, int left, int lidar_data) {
+		System.out.println(data.toString());
 
-        data.add(front_radar.checkReading(front));
-        data.add(rear_radar.checkReading(rear));
-        data.add(left_radar.checkReading(left));
-        data.add(lidar.checkReading(lidar_data));
-
-        System.out.println(data.toString());
-        return data;
-    }
+		return data;
+	}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     public void changeLane() {
 
-        if (isEmpty && lane_pos!=3) {
-            moveForward();
-            lane_pos ++;
-        } else {
-
-            moveForward();
-        }
+    	actuator.changeLane(this);
 
     }
 
-    public Point whereIs(){
-        Point coordinates = new Point();
-        coordinates.x = current_position;
-        coordinates.y = lane_pos;
+    //checks in which lane the car is
+    /**
+    Point whereIs() {
+
+        return Coordinates;
+    }
+     **/
+
+    public ArrayList <Integer>  whereIs (){
+        ArrayList <Integer> coordinates =new ArrayList <>();
+        coordinates.add(current_position);
+        coordinates.add(lane_pos);
+
         return coordinates;
     }
 }
